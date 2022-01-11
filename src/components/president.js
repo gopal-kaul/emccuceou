5183b6219f0cf9f829b9dc39817fb378fc2a7c23
Gopal Kaul <gopalkaul5@gmail.com>
2022-01-11 20:14:53 +0530

    Added My Message

--- src/components/president.js
+++ src/components/president.js
@@ -17,14 +17,29 @@ export default function President() {
         />
       </div>
       <div className="text-center">
-        <em>Gopal Kaul</em>
-        <p>
-          Engineers' Music And Cultural Club is the largest non-technical club
-          in the college! I am very grateful to be the fourth president of the
-          club!
-        </p>
+        <em style={{ fontSize: "3em" }}>Gopal Kaul</em>
       </div>
-      <div className="main-text"></div>
+      <p style={{fontSize:"1.5rem"}}>
+        Engineers' Music And Cultural Club is the largest non-technical club in
+        the college, and I am very grateful that I have been given the golden
+        opportunity of being the club's President and entrusted with this
+        responsibility. The club has grown exponentially since my first year.
+        From 5 in a Jam Session to 80, from 50 in an Open Mic to 400, the
+        importance of the Club in the college has been ever-increasing. I have
+        been very proud of where the Club is today, and I hope it grows with the
+        same rate forever. Our Principal has been very supporting. He got our
+        room repaired and renovated, and he has been super supportive in all the
+        events we have organised.
+        <br />
+        <br />
+        I've always been interested in music. I am a self-learned Guitar player
+        and I've been playing since the past 5 years now. Music is always a part
+        of what I do. Some of the artists whose music really moves and
+        influences me are Ed Sheeran, Shawn Mendes, The Beatles, Queen and
+        Avicii!
+        <br />
+        <br />I hope to see the Club achieve a lot more in the coming years!
+      </p>
     </Container>
   );
 }
