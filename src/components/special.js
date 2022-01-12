f1166f823b045f90c943a24931d05bc2ee83a2a7
Gopal Kaul <gopalkaul5@gmail.com>
2022-01-12 22:57:59 +0530

    Added all special mentions

--- src/components/special.js
+++ src/components/special.js
@@ -1,35 +1,346 @@
 import React from "react";
-import { Card, Image, Container } from "react-bootstrap";
+import { Card, Image, Container, Row, Col } from "react-bootstrap";
 export default function Specialmentions() {
   return (
     <Container style={{ marginTop: "2%", marginBottom: "2%" }}>
       <h1>Special Mentions Page</h1>
       <hr />
-      <Card style={{ margin: 5 }}>
-        <Image
-          variant="top"
-          src="images/special/nathan.jpg"
-          width={200}
-          height={200}
-          style={{ marginTop: "2%", marginLeft: "2%" }}
-          roundedCircle
-          fluid
-        />
-        <Card.Body>
-          <Card.Title style={{ fontSize: "25px" }}>
-            Nathan Arul Thadmalla
-          </Card.Title>
-          <Card.Text>
-            I started singing like any average Christian child in church 😂.
-            When I was in 11th grade I started writing poetry which slowly
-            developed into me writing songs and my guitar journey started when I
-            was 12 years old. Currently I had one show for my originals thanks
-            to the music club, was the founder of the open mic in college and
-            currently working on original music to be released soon . Apart from
-            this I am also a part time guitar teacher
-          </Card.Text>
-        </Card.Body>
-      </Card>
+      <div className="card-centre">
+        <Row>
+          <Col>
+            <Card style={{ margin: 5 }}>
+              <Image
+                variant="top"
+                src="images/special/nathan.jpg"
+                width={200}
+                height={200}
+                style={{ marginTop: "2%", marginLeft: "2%" }}
+                roundedCircle
+                fluid
+              />
+              <Card.Body>
+                <Card.Title style={{ fontSize: "25px" }}>
+                  Nathan Arul Thadmalla
+                </Card.Title>
+                <Card.Text>
+                  I started singing like any average Christian child in church
+                  😂. When I was in 11th grade I started writing poetry which
+                  slowly developed into me writing songs and my guitar journey
+                  started when I was 12 years old. Currently I had one show for
+                  my originals thanks to the music club, was the founder of the
+                  open mic in college and currently working on original music to
+                  be released soon . Apart from this I am also a part time
+                  guitar teacher
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col lg={2}>
+            <Image src="images/gifs/music3.gif" width={200} height={200} />
+          </Col>
+        </Row>
+      </div>
+      <div className="card-centre">
+        <Row>
+          <Col lg={2}>
+            <Image src="images/gifs/music4.gif" width={200} height={200} />
+          </Col>
+          <Col>
+            <Card style={{ margin: 5 }}>
+              <Image
+                variant="top"
+                src="images/special/surabhi.jpg"
+                width={200}
+                height={200}
+                style={{ marginTop: "2%", marginLeft: "2%" }}
+                roundedCircle
+                fluid
+              />
+              <Card.Body>
+                <Card.Title style={{ fontSize: "25px" }}>
+                  Surabhi Nadigotti
+                </Card.Title>
+                <Card.Text>
+                  I started learning carnatic music vocals when I was 7. Back
+                  then, I wasn't really into music. But with time, I slowly
+                  started developing my interest towards it. Although I had to
+                  change my classical carnatic trainers for thrice and had to
+                  stop taking the training, I would still love to continue
+                  pursuing it. As a child, I didn't really understand the beauty
+                  of classical music, but on exploring other genres too growing
+                  up, I realised how rich and authentic classical carnatic music
+                  is! Apart from Carnatic music, I'd love listening to Sufi
+                  songs and Contemporary movie songs. My favourite artists
+                  include Ayushmaan Khurrana, Shreya Ghoshal, A.R. Rehman, Sid
+                  Sreeram, Rahul Vellal, Chinmayi Sripada, and many more. Coming
+                  to the western music, I don't know many artists, as I didn't
+                  discover my interests particularly here in the western. But so
+                  far, my favourites are- Taylor Swift, Avicii, and Chester
+                  Bennington.
+                  <br />
+                  Talking about my association with the club, I feel totally
+                  jubilant to have known the people of the club! It was always
+                  enthralling to sing along, rehearse and practice with these
+                  fabulous bunch of people! And, there was never a moment off!{" "}
+                  <br />
+                  Also, the amount of takeaways I had from being a part of
+                  sessions of the club, was really great! Because, when you
+                  practice with people you get to know enormous details,
+                  starting from the scale of the song to the very minute details
+                  like the vibrato!
+                  <br />
+                  EMCC has definitely helped me evolve as a better singer! P.S.
+                  I also, started learning to play the guitar and the keyboard
+                  (which I stopped learning in my 5th grade :/)
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </div>
+      <div className="card-centre">
+        <Row>
+          <Col>
+            <Card style={{ margin: 5 }}>
+              <Image
+                variant="top"
+                src="images/special/abhinav.jpg"
+                width={200}
+                height={200}
+                style={{ marginTop: "2%", marginLeft: "2%" }}
+                roundedCircle
+                fluid
+              />
+              <Card.Body>
+                <Card.Title style={{ fontSize: "25px" }}>
+                  Abhinava Karthikeya
+                </Card.Title>
+                <Card.Text>
+                  My association with music is one of the turning points in my
+                  life. When I was a child I was bad at everything (
+                  studies,sports etc). During my 4th standard there were few
+                  competitions in our school and I had to participate in it as
+                  it was compulsory for every one to participate in any one of
+                  them. I chose to participate in singing and I won second
+                  prize.
+                  <br />
+                  This helped me to boost my confidence and improve myself. I am
+                  not a trained singer but I would love to learn carnatic music
+                  and piano. My favourite artists include Anurag Kulkarni, Sid
+                  Sriram, Sunitha , Arjith Singh and many more.
+                  <br />
+                  Emcc helped me a lot in improving my skills.It gave me a
+                  chance to perform in many events,chance to meet many good
+                  musicians and share the stage with them. I feel excited being
+                  part of emcc and looking forward to participating in more
+                  events.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col lg={2}>
+            <Image src="images/gifs/music5.gif" width={200} height={200} />
+          </Col>
+        </Row>
+      </div>
+      <div className="card-centre">
+        <Row>
+          <Col lg={2}>
+            <Image src="images/gifs/music1.gif" width={200} height={200} />
+          </Col>
+          <Col>
+            <Card style={{ margin: 5 }}>
+              <Image
+                variant="top"
+                src="images/special/vaishnavi.jpg"
+                width={200}
+                height={200}
+                style={{ marginTop: "2%", marginLeft: "2%" }}
+                roundedCircle
+                fluid
+              />
+              <Card.Body>
+                <Card.Title style={{ fontSize: "25px" }}>
+                  Vaishnavi Gannoji
+                </Card.Title>
+                <Card.Text>
+                  Hey, I am Vaishnavi. Here is my story. My music journey
+                  started when I was in my second standard, my parents joined me
+                  in a music institute nearby my school because there was no one
+                  to look after me as both of them had a job, I eventually
+                  developed interest in carnatic music, I love listening to all
+                  kind of genres, melody being my favorite and music singers
+                  like Shreya Ghoshal, Sid Sriram and music directors like A.R.
+                  Rahman, M.M Keeravani made me love music even more with their
+                  mesmerizing work in the music industry.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </div>
+      <div className="card-centre">
+        <Row>
+          <Col>
+            <Card style={{ margin: 5 }}>
+              <Image
+                variant="top"
+                src="images/special/kiran.jpg"
+                width={200}
+                height={200}
+                style={{ marginTop: "2%", marginLeft: "2%" }}
+                roundedCircle
+                fluid
+              />
+              <Card.Body>
+                <Card.Title style={{ fontSize: "25px" }}>
+                  Kiran Ganji
+                </Card.Title>
+                <Card.Text>
+                  Who doesn't love listening to music?!.. Even though I was a
+                  melophile from my childhood. I always used to listen to music
+                  and sing in front of my friends without knowing anything about
+                  music..just feeling the emotion of the song but I wasn't able
+                  to sing with that emotion. When I was in 6th standard, I saw
+                  one girl.She was so beautiful and smart as well. I don't know
+                  why..I used to feel happy around her. Gradually I fell in love
+                  with her. But I didn't have the courage to tell her . So I
+                  thought I could express my feelings through music. So I
+                  started exploring music and really enjoyed learning little
+                  basics .I sang "Naalone pongenu narmada" for her. She loved it
+                  and appreciated me . I was on cloud nine. Finally I proposed
+                  to her in 10th standard but she told me that she loves someone
+                  else. That really destroyed me mentally. I went into
+                  depression.Well.. that helped me in one way. After that
+                  incident, I was able to sing with emotion. Many people
+                  appreciated my voice and my way of singing. She's the main
+                  reason behind the 'emotion' in my singing . So yeah!.. that's
+                  how I got into music and I'm loving learning it ! Thanks to my
+                  first love &lt;3
+                  <br />
+                  My favourite artists are A.R.Rahman,G.V.Prakash,Armaan malik I
+                  listen to almost all genres of music cause I'm a melophile
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col lg={1}>
+            <Image src="images/gifs/music2.gif" width={200} height={200} />
+          </Col>
+        </Row>
+      </div>
+      <div className="card-centre">
+        <Row>
+          <Col lg={2}>
+            <Image src="images/gifs/music6.gif" width={200} height={200} />
+          </Col>
+          <Col>
+            <Card style={{ margin: 5 }}>
+              <Image
+                variant="top"
+                src="images/special/keerthana.jpg"
+                width={200}
+                height={200}
+                style={{ marginTop: "2%", marginLeft: "2%" }}
+                roundedCircle
+                fluid
+              />
+              <Card.Body>
+                <Card.Title style={{ fontSize: "25px" }}>
+                  Keerthana Reddy
+                </Card.Title>
+                <Card.Text>
+                  I was seven , when we moved to Gujarat. New place , new
+                  language , new culture but my love for the music stayed the
+                  same. I started learning hindustani music there and continued
+                  to do so until I moved to hyderabad. Though my music teacher
+                  taught me in gujarati , language was never a barrier for me to
+                  learn music. However, I lacked confidence to perform in front
+                  of an audience . I used to just sing in front of my mother,
+                  and she kept on forcing me to perform at family gatherings,
+                  but I always had an excuse. I've posted a few covers during
+                  the pandemic, and the response has boosted my confidence.
+                  Since then, I've been an active participant in emcc's events.
+                  I listen to a variety of genres but Sid Sriram's and Shawn
+                  Mendes songs are what I crave the most!.I'm looking forward to
+                  taking part in upcoming events, and I'll surely give it my
+                  all!
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </div>
+      <div className="card-centre">
+        <Row>
+          <Col>
+            <Card style={{ margin: 5 }}>
+              <Image
+                variant="top"
+                src="images/special/pratik.jpg"
+                width={200}
+                height={200}
+                style={{ marginTop: "2%", marginLeft: "2%" }}
+                roundedCircle
+                fluid
+              />
+              <Card.Body>
+                <Card.Title style={{ fontSize: "25px" }}>Pratik</Card.Title>
+                <Card.Text>
+                  My mom made me join a guitar class for a few months but I
+                  learnt most of it in 2020 lockdown My favourite artists are
+                  The weeknd , Guns n Roses , maroon 5, one direction, kanye ,
+                  future , migos , lil uzi vert , arijit singh. My music
+                  interests are Hiphop, R&amp;b
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col lg={2}>
+            <Image src="images/gifs/music8.gif" width={200} height={200} />
+          </Col>
+        </Row>
+      </div>
+      <div className="card-centre">
+        <Row>
+          <Col lg={2}>
+            <Image src="images/gifs/music7.gif" width={200} height={200} />
+          </Col>
+          <Col>
+            <Card style={{ margin: 5 }}>
+              <Image
+                variant="top"
+                src="images/special/shishira.jpg"
+                width={200}
+                height={200}
+                style={{ marginTop: "2%", marginLeft: "2%" }}
+                roundedCircle
+                fluid
+              />
+              <Card.Body>
+                <Card.Title style={{ fontSize: "25px" }}>Shishira</Card.Title>
+                <Card.Text>
+                  Hey...! I am Shishira. Here is a short glimpse of my music
+                  journey. I initiated my musical journey from an early age of
+                  mine with the tunes of Carnatic music. To advance my musical
+                  interest, I had done a certificate course in Carnatic music
+                  from the prestigious Pottisreeramulu Telugu University. I took
+                  part in carnatic concerts on the occasion of Sri Ramanavami
+                  and Thyagaraja Aradhana. I also extended my Carnatic music
+                  interest by participating in the bhajan Samrat event. I love
+                  Shreya Ghoshal's voice tone and singing style moving to the
+                  cinematic genre. I believe in a quote-
+                  <br />
+                  <q>
+                    Music is the literature of the heart; it commences where
+                    speech ends. - Alphonse de Lamartine
+                  </q>
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </div>
     </Container>
   );
 }
