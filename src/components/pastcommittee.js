c8e6fce2bcf8171ff50ef0558c10afd6af1551e2
Gopal Kaul <gopalkaul5@gmail.com>
2022-01-22 01:50:14 +0530

    Added new component for Past Committees

---
+++ src/components/pastcommittee.js
@@ -0,0 +1,163 @@
+import React from "react";
+import { Container, Table } from "react-bootstrap";
+export default function PastCommittee() {
+  return (
+    <Container style={{ marginTop: "5%", marginBottom: "5%" }}>
+      <h1>EMC² Executive Committee 2020-21</h1>
+
+      <Table hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Post</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1.</td>
+            <td>Krishna Hasitha</td>
+            <td>President</td>
+          </tr>
+          <tr>
+            <td>2.</td>
+            <td>Ragini Kaul</td>
+            <td>Vice-President</td>
+          </tr>
+          <tr>
+            <td>3.</td>
+            <td>Nathan Thadmalla</td>
+            <td>Band Leader</td>
+          </tr>
+          <tr>
+            <td>4.</td>
+            <td>Munnaluru Akhila</td>
+            <td>General Secretary</td>
+          </tr>
+          <tr>
+            <td>5.</td>
+            <td>Gopal Kaul</td>
+            <td>Joint Secretary</td>
+          </tr>
+          <tr>
+            <td>6.</td>
+            <td>Pratick</td>
+            <td>Joint Secretary</td>
+          </tr>
+          <tr>
+            <td>7.</td>
+            <td>M Niharika</td>
+            <td>Joint Treasurer</td>
+          </tr>
+          <tr>
+            <td>8.</td>
+            <td>Aniral Rao Adaveni</td>
+            <td>Assistant Band Leader</td>
+          </tr>
+          <tr>
+            <td>9.</td>
+            <td>Supriya Rathod</td>
+            <td>Executive Committee Member</td>
+          </tr>
+          <tr>
+            <td>10.</td>
+            <td>Chinthamalla Shiney</td>
+            <td>Executive Committee Member</td>
+          </tr>
+          <tr>
+            <td>11.</td>
+            <td>Annimalla Manaswini</td>
+            <td>Executive Committee Member</td>
+          </tr>
+        </tbody>
+      </Table>
+      <br />
+      <h1>EMC² Executive Committee 2019-20</h1>
+
+      <Table hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Post</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1.</td>
+            <td>A. S. Sarthak</td>
+            <td>President</td>
+          </tr>
+          <tr>
+            <td>2.</td>
+            <td>D. L. Prachotan</td>
+            <td>Vice President</td>
+          </tr>
+          <tr>
+            <td>3.</td>
+            <td>Sreekar Vibhas</td>
+            <td>Secretary</td>
+          </tr>
+          <tr>
+            <td>4.</td>
+            <td>Keerthana Kyasa</td>
+            <td>Treasurer</td>
+          </tr>
+          <tr>
+            <td>5.</td>
+            <td>Sanjana Sapasotty</td>
+            <td>Joint Secretary</td>
+          </tr>
+          <tr>
+            <td>6.</td>
+            <td>Krishna Hasitha</td>
+            <td>Core Member</td>
+          </tr>
+          <tr>
+            <td>7.</td>
+            <td>Nathan Thadmalla</td>
+            <td>Core Member</td>
+          </tr>
+          <tr>
+            <td>8.</td>
+            <td>Ragini Kaul</td>
+            <td>Core Member</td>
+          </tr>
+        </tbody>
+      </Table>
+      <br />
+      <h1>EMC² Executive Committee 2018-19</h1>
+      <Table hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Post</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1.</td>
+            <td>Lindsay B Charles</td>
+            <td>President</td>
+          </tr>
+          <tr>
+            <td>2.</td>
+            <td>D. L. Prachotan</td>
+            <td>Vice President</td>
+          </tr>
+          <tr>
+            <td>3.</td>
+            <td>A. S. Sarthak</td>
+            <td>Treasurer</td>
+          </tr>
+          <tr>
+            <td>4.</td>
+            <td>Shreya Reddy Kotha</td>
+            <td>Secretary</td>
+          </tr>
+        </tbody>
+      </Table>
+    </Container>
+  );
+}
