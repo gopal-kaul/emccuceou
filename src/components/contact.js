27a29d3873947c0687ed2f49fbe5a2c6ed6b3dd4
Gopal Kaul <gopalkaul5@gmail.com>
2022-01-26 20:57:45 +0530

    Fixed indent

--- src/components/contact.js
+++ src/components/contact.js
@@ -32,7 +32,7 @@ export default function Contact() {
           </div>
           <div className="col-lg-7">
             <iframe
-            title="Maps Location"
+              title="Maps Location"
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3807.116329436644!2d78.51728491487663!3d17.406204088067316!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcb992a61089b8b%3A0x6860898df38073c0!2sEngineers&#39;%20Music%20and%20Cultural%20Club!5e0!3m2!1sen!2sin!4v1584853736672!5m2!1sen!2sin"
               width="600"
               height="400"
