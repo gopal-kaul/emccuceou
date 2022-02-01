b2f21b2ffffb7555a7dbd796c99ce711dd8bc2c0
Gopal Kaul <gopalkaul5@gmail.com>
2022-02-01 22:13:08 +0530

    Added new spotlight route to navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,6 +1,6 @@
 import { NavDropdown, Container, Nav, Navbar, Image } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
-export default function MyNavbar({ selected }) {
+export default function MyNavbar() {
   return (
     <>
       <Navbar collapseOnSelect expand="lg" className="mynav" variant="dark">
@@ -11,7 +11,7 @@ export default function MyNavbar({ selected }) {
                 width={45}
                 height={50}
                 src="/images/logo_white.png"
-                alt="EMCC Logo"
+                alt="EMC² Logo"
               />
             </Navbar.Brand>
           </LinkContainer>
@@ -34,16 +34,40 @@ export default function MyNavbar({ selected }) {
                 <LinkContainer to={"/special"}>
                   <NavDropdown.Item>Special Mentions</NavDropdown.Item>
                 </LinkContainer>
-                <LinkContainer to={"/spotlight"}>
-                  <NavDropdown.Item>Spotlight</NavDropdown.Item>
+                <LinkContainer to={"/singers"}>
+                  <NavDropdown.Item>Spotlight (Singers)</NavDropdown.Item>
+                </LinkContainer>
+                <LinkContainer to={"/dancers"}>
+                  <NavDropdown.Item>Spotlight (Dancers)</NavDropdown.Item>
                 </LinkContainer>
               </NavDropdown>
               <LinkContainer to={"/band"}>
                 <Nav.Link>The Band</Nav.Link>
               </LinkContainer>
-              <LinkContainer to={"/events"}>
-                <Nav.Link>Events</Nav.Link>
-              </LinkContainer>
+              <NavDropdown title="Events" id="collasible-nav-dropdown">
+                <NavDropdown.Item href="/events#openmic">
+                  Open Mic
+                </NavDropdown.Item>
+                <NavDropdown.Item href="/events#raaga">RAAGA</NavDropdown.Item>
+                <NavDropdown.Item href="/events#jam">
+                  Jam Session
+                </NavDropdown.Item>
+                <NavDropdown.Item href="/events#gettoknow">
+                  Get to Know EMC²
+                </NavDropdown.Item>
+                {/* <LinkContainer to={"/events#openmic"}>
+                  <NavDropdown.Item>Open Mic</NavDropdown.Item>
+                </LinkContainer>
+                <LinkContainer to={"/events#raaga"}>
+                  <NavDropdown.Item>RAAGA</NavDropdown.Item>
+                </LinkContainer>
+                <LinkContainer to={"/events#jam"}>
+                  <NavDropdown.Item>Jam Session</NavDropdown.Item>
+                </LinkContainer>
+                <LinkContainer to={"/events#gettoknow"}>
+                  <NavDropdown.Item>Get to Know EMC²</NavDropdown.Item>
+                </LinkContainer> */}
+              </NavDropdown>
               <LinkContainer to={"/contact"}>
                 <Nav.Link>Contact Us</Nav.Link>
               </LinkContainer>
